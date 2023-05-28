3d9ec36fb218024ba1852ca05ef14d027947785f
Limeshrk <67397781+Limeshrk@users.noreply.github.com>
2023-05-28 12:04:39 +0200

    6. feladat (filter fields)

--- app.py
+++ app.py
@@ -9,13 +9,13 @@ app = Flask(__name__)
 
 # Load the projects data from a pickle file
 def initialize_data():
-  #check if pickle exist
+  #check if pickle exist and loads the data
   if os.path.isfile('projects.pickle'):
     with open('projects.pickle', 'rb') as f:
       projects_data = pickle.load(f)
     return projects_data
   else: #if not exist
-    create_pickle() #create it
+    create_pickle() #create it and loads the data
     with open('projects.pickle', 'rb') as f:
       projects_data = pickle.load(f)
     return projects_data
@@ -27,17 +27,23 @@ def save_data(data):
   with open('projects.pickle', 'wb') as f:
     pickle.dump(data, f)
 
+
 @app.route("/")
 def home():
   return render_template("index.html.j2", name="Zsolt")
 
-
 @app.route("/projects")
 def get_projects():
-  return jsonify({'projects': projects}), 200, {
-      # add Access-Control-Allow-Origin header
-      'Access-Control-Allow-Origin': 'http://127.0.0.1:8080'
-  }
+  try:
+    request_data = request.get_json() #checks for JSON in request
+    if 'fields' in request_data:
+      fields = request_data['fields']
+      filtered_projects = filter_list_of_dicts(projects['projects'], fields)
+      return jsonify(filtered_projects), 200
+  except:
+    pass  # if no JSON in the request, returns unfilterd data
+  return jsonify(projects), 200
+
 
 
 @app.route("/project", methods=['POST'])
@@ -101,6 +107,22 @@ def get_project_tasks(name):
   return jsonify({'message': 'project not found'}), 404
 
 
+@app.route('/project/<string:project_id>/task', methods=['GET'])
+def get_all_tasks_in_project(project_id):
+  for project in projects['projects']:
+    if project['project_id'] == project_id:
+      try:
+        request_data = request.get_json() #checks for JSON in request
+        if 'fields' in request_data:
+          fields = request_data['fields']
+          filtered_tasks = filter_list_of_dicts(project['tasks'], fields)
+          return jsonify({'tasks':filtered_tasks}), 200
+      except:
+        pass  # if no JSON in the request, returns unfilterd data
+      return jsonify({'tasks':project['tasks']}), 200
+  return jsonify({'message': 'Project not found'}), 404
+
+
 @app.route("/project/<string:project_id>/task", methods=['POST'])
 def add_task_to_project(project_id):
   request_data = request.get_json()
@@ -126,3 +148,15 @@ def add_task_to_project(project_id):
       save_data(projects)
       return jsonify({'message': f'task created with id: {new_task_id}'}), 201
   return jsonify({'message': 'project not found'}), 404
+
+
+# Create the filtered_dicts
+def filter_list_of_dicts(list_of_dicts, fields):
+    filtered_dicts = []
+    for original_dict in list_of_dicts:
+        dict_copy = original_dict.copy()
+        for key in original_dict.keys():
+            if key not in fields:
+                del dict_copy[key]
+        filtered_dicts.append(dict_copy)
+    return filtered_dicts
